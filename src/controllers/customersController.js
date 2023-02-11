3f45ef4615162dd95a67840f6f6a601f3b29600f
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-02-11 10:57:55 -0300

    Rota PUT/customers/:id com dados da request validados

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -44,12 +44,12 @@ export async function postCustomer(req, res) {
 
     const customerAlreadyExists = await db.query('SELECT * FROM customers WHERE cpf = $1', [cpf]);
 
+
     try {
 
         if (customerAlreadyExists.rows.length != 0) return res.sendStatus(STATUS_CODE.CONFLICT);
 
-        // await db.query('INSERT INTO (name , phone, cpf , birthday) VALUES ($1 , $2 , $3, $4)',
-        //     [name, phone, cpf, birthday]);
+        await db.query('INSERT INTO customers (name , phone, cpf , birthday) VALUES ($1 , $2 , $3, $4)', [name, phone, cpf, birthday]);
 
         res.status(STATUS_CODE.CREATED).send({ name, phone, cpf, birthday });
 
@@ -58,4 +58,33 @@ export async function postCustomer(req, res) {
         res.status(STATUS_CODE.SERVER_ERROR).send(error);
 
     }
+}
+
+export async function putCustomer(req, res) {
+
+    const { id } = req.params;
+
+    const { name, phone, cpf, birthday } = req.body;
+
+    const customerToUpdate = await db.query('SELECT * FROM customers WHERE id = $1', [id]);
+
+    try {
+
+        if (customerToUpdate.rows.length === 0) return res.sendStatus(STATUS_CODE.NOT_FOUND);
+
+        if (customerToUpdate.rows[0].cpf != cpf) {
+            const cpfExists = await db.query('SELECT * FROM customers WHERE cpf = $1', [cpf]);
+            if (cpfExists.rows.length != 0) return res.sendStatus(STATUS_CODE.CONFLICT);
+        }
+
+        await db.query('UPDATE customers SET name = $1, cpf = $2, phone = $3, birthday = $4 WHERE id = $5', [name, cpf, phone, birthday, id]);
+
+        res.sendStatus(200);
+
+    } catch (error) {
+
+        res.status(STATUS_CODE.SERVER_ERROR).send(error);
+
+    }
+
 }
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCustomers, getCustomersById, postCustomer } from "../controllers/customersController.js";
+import { getCustomers, getCustomersById, postCustomer, putCustomer } from "../controllers/customersController.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { postCustomerSchema } from "../schemas/postCustomerSchema.js";
 
@@ -8,5 +8,6 @@ const customersRouter = Router();
 customersRouter.get('/customers', getCustomers);
 customersRouter.get('/customers/:id', getCustomersById);
 customersRouter.post('/customers', validateSchema(postCustomerSchema, 400), postCustomer);
+customersRouter.put('/customers/:id', validateSchema(postCustomerSchema, 400), putCustomer);
 
 export { customersRouter };
