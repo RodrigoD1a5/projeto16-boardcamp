7607134a8fe086b1c79d6625b03260897ed789b9
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-02-11 09:27:53 -0300

    Rota GET/customers/:id

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -16,3 +16,24 @@ export async function getCustomers(req, res) {
 
     }
 }
+
+export async function getCustomersById(req, res) {
+
+    const { id } = req.params;
+
+    const customer = await db.query('SELECT * FROM customers WHERE id = $1', [id]);
+
+    try {
+
+        if (customer.rows.length === 0) return res.sendStatus(STATUS_CODE.NOT_FOUND);
+
+        res.status(STATUS_CODE.OK).send(customer.rows[0]);
+
+
+    } catch (error) {
+
+        res.status(STATUS_CODE.SERVER_ERROR).send(error);
+
+    }
+
+}
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getCustomers } from "../controllers/customersController.js";
+import { getCustomers, getCustomersById } from "../controllers/customersController.js";
 
 const customersRouter = Router();
 
 customersRouter.get('/customers', getCustomers);
+customersRouter.get('/customers/:id', getCustomersById);
 
 export { customersRouter };
