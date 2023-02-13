416e6d47f16c5bf8fb0cb870b1bff3e85283f908
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-02-13 12:20:28 -0300

    Verificando existência do game pelo id

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,20 +1,27 @@
 import dayjs from "dayjs";
+import { db } from "../database/database.connection.js";
 import { STATUS_CODE } from "../enums/statusCode.js";
 
 export async function postRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body;
 
-    const rental = {
-        customerId,
-        gameId,
-        daysRented,
-        rentDate: dayjs().format("YYYY-MM-DD"),
-        originalPrice: daysRented * 3,
-        returnDate: null,
-        delayFee: null
-    };
-
     try {
+
+        const game = await db.query('SELECT * FROM games WHERE id = $1', [gameId]);
+        if (game.rows.length === 0) return res.sendStatus(STATUS_CODE.BAD_REQUEST);
+        const pricePerDay = game.rows[0].pricePerDay;
+
+
+        const rental = {
+            customerId,
+            gameId,
+            daysRented,
+            rentDate: dayjs().format("YYYY-MM-DD"),
+            originalPrice: daysRented * pricePerDay,
+            returnDate: null,
+            delayFee: null
+        };
+
 
         res.send(rental);
 
